7675d8b295700b5e5e540c744fb2497b7bc198fa
Chiara Paci <chiara.paci@gmail.com>
2017-05-16 10:50:43 +0200

    New app archive

--- baskervilleweb/archive/management/commands/load_photo.py
+++ baskervilleweb/archive/management/commands/load_photo.py
@@ -8,16 +8,46 @@ from django.contrib.auth.password_validation import validate_password
 from django.core import exceptions
 from django.core.management.base import BaseCommand, CommandError
 
+from PIL import Image
+from archive import models
+
+import os.path,os
+
 class Command(BaseCommand):
     help = 'Used to import a photo.'
     requires_migrations_checks = True
 
     def add_arguments(self, parser):
-        parser.add_argument(
-            'photo',
-            dest="photo", 
-        )
+        parser.add_argument('photo')
 
     def handle(self, *args, **options):
-        pass
+        photo=options["photo"]
+
+        dirphoto=os.path.dirname(photo)
+        fname=os.path.basename(photo)
+
+        fname,ext=os.path.splitext(fname)
+
+        print(photo)
+
+        reldir=os.path.relpath(dirphoto,models.PHOTO_ARCHIVE_FULL)
+
+        thumb_fname=os.path.join(models.PHOTO_ARCHIVE_THUMB,reldir,fname+".jpeg")
+
+        os.makedirs(os.path.join(models.PHOTO_ARCHIVE_THUMB,reldir),exist_ok=True)
+
+        print(thumb_fname)
+        try:
+            im = Image.open(photo)
+            im.thumbnail( (128,128) )
+            im.save(thumb_fname, "JPEG")
+            im.close()
+        except IOError:
+            print("cannot create thumbnail for", photo)
+
+        im = Image.open(photo)
+        
+        print(dir(im))
+        print(im.format,im.format_description,im.size,im.width,im.height,im.info,im.mode)
+        
 
--- baskervilleweb/archive/models.py
+++ baskervilleweb/archive/models.py
@@ -5,10 +5,21 @@ from django.db import models
 PHOTO_ARCHIVE_FULL = "/home/chiara/foto/full"
 PHOTO_ARCHIVE_THUMB = "/home/chiara/foto/thumb"
 
+class ImageFormat(models.Model):
+    name = models.CharField(max_length=1024,unique=True)
+    description = models.CharField(max_length=8192)
+
+    def __str__(self): return self.name
+
 class Photo(models.Model):
     full_path =  models.FilePathField(path=PHOTO_ARCHIVE_FULL,recursive=True,max_length=1024)
     thumb_path = models.FilePathField(path=PHOTO_ARCHIVE_THUMB,recursive=True,max_length=1024)
     description = models.CharField(max_length=8192,blank=True)
+    width = models.IntegerField()
+    height = models.IntegerField()
+    format = models.ForeignKey(ImageFormat)
+    mode = models.CharField(max_length=1024)
+
     type = models.CharField(max_length=1024)
 
     def __str__(self): return self.full_path
