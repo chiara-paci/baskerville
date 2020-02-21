9676f2cc4ac860ada83ececf84baddede72042ad
Chiara Paci <chiara.paci@gmail.com>
2020-02-21 18:10:24 +0100

    Added X-Accel-Redirect to Photo views

--- baskervilleweb/archive/views.py
+++ baskervilleweb/archive/views.py
@@ -16,49 +16,68 @@ class ImageMixin(object):
             return img.rotate(-90,expand=True)
         return img.rotate(90,expand=True)
 
-class PhotoImageView(DetailView,ImageMixin):
+class PhotoImageView(DetailView):
     model = models.Photo
-    
-    def get_image_format(self,ext):
-        if ext in [ "jpg", "jpeg" ]: return "image/jpeg","JPEG"
-        if ext in ["png","gif","tiff"]: 
-            return "image/%s" % ext,ext.upper()
-        return None,None
 
     def get(self,request,*args,**kwargs):
         photo=self.get_object()
-        ext=kwargs["ext"].lower()
-        mimetype,savetype=self.get_image_format(ext)
-        img=Image.open(photo.full_path)
-        new_img=self.adjust_photo(img,photo)
-
-        if not mimetype: mimetype=photo.mimetype
-        response = HttpResponse(content_type=mimetype)
-        if savetype:
-            new_img.save(response, savetype)
-        else:
-            new_img.save(response)
-        img.close()
+        response = HttpResponse()
+        response["Content-Disposition"] = "attachment; filename={0}".format(photo.image_url())
+        response['X-Accel-Redirect'] = photo.full_path
         return response
 
-class PhotoThumbView(DetailView,ImageMixin):
+class PhotoThumbView(DetailView):
     model = models.Photo
-    
-
-    def get_image_format(self):
-        mimetype="image/jpeg"
-        savetype="JPEG"
-        return mimetype,savetype
 
     def get(self,request,*args,**kwargs):
         photo=self.get_object()
-        img=Image.open(photo.thumb_path)
-        mimetype,savetype=self.get_image_format()
-        new_img=self.adjust_photo(img,photo)
-        response = HttpResponse(content_type=mimetype)
-        new_img.save(response, savetype)
-        img.close()
+        response = HttpResponse()
+        response["Content-Disposition"] = "attachment; filename={0}".format(photo.thumb_url())
+        response['X-Accel-Redirect'] = photo.thumb_path
         return response
+
+# class PhotoImageView(DetailView,ImageMixin):
+#     model = models.Photo
+    
+#     def get_image_format(self,ext):
+#         if ext in [ "jpg", "jpeg" ]: return "image/jpeg","JPEG"
+#         if ext in ["png","gif","tiff"]: 
+#             return "image/%s" % ext,ext.upper()
+#         return None,None
+
+#     def get(self,request,*args,**kwargs):
+#         photo=self.get_object()
+#         ext=kwargs["ext"].lower()
+#         mimetype,savetype=self.get_image_format(ext)
+#         img=Image.open(photo.full_path)
+#         new_img=self.adjust_photo(img,photo)
+
+#         if not mimetype: mimetype=photo.mimetype
+#         response = HttpResponse(content_type=mimetype)
+#         if savetype:
+#             new_img.save(response, savetype)
+#         else:
+#             new_img.save(response)
+#         img.close()
+#         return response
+
+# class PhotoThumbView(DetailView,ImageMixin):
+#     model = models.Photo
+
+#     def get_image_format(self):
+#         mimetype="image/jpeg"
+#         savetype="JPEG"
+#         return mimetype,savetype
+
+#     def get(self,request,*args,**kwargs):
+#         photo=self.get_object()
+#         img=Image.open(photo.thumb_path)
+#         mimetype,savetype=self.get_image_format()
+#         new_img=self.adjust_photo(img,photo)
+#         response = HttpResponse(content_type=mimetype)
+#         new_img.save(response, savetype)
+#         img.close()
+#         return response
 
 class Filter(object):
 
