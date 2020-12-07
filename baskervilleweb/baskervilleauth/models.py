f751043f237bbbe375becdd38d8ed61cda67a4d9
Chiara Paci <chiara.paci@gmail.com>
2020-12-07 14:57:32 +0100

    Baskerville Auth

--- baskervilleweb/baskervilleauth/models.py
+++ baskervilleweb/baskervilleauth/models.py
@@ -2,14 +2,15 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    date_of_birth = models.DateField()
-    gender = models.CharField(max_length=128,choices=[ ("male","male"),
-                                                       ("female","female") ])
-    height = models.PositiveIntegerField(verbose_name='height (cm)',default=160)
-    # https://en.wikipedia.org/wiki/Harris%E2%80%93Benedict_equation
-    lifestyle = models.CharField(max_length=128,choices=[ ("sedentary","sedentary"),     
-                                                          ("active","active"),           
-                                                          ("very active","very active")])
+    pass
+    # date_of_birth = models.DateField()
+    # gender = models.CharField(max_length=128,choices=[ ("male","male"),
+    #                                                    ("female","female") ])
+    # height = models.PositiveIntegerField(verbose_name='height (cm)',default=160)
+    # # https://en.wikipedia.org/wiki/Harris%E2%80%93Benedict_equation
+    # lifestyle = models.CharField(max_length=128,choices=[ ("sedentary","sedentary"),     
+    #                                                       ("active","active"),           
+    #                                                       ("very active","very active")])
 
     
     # class Meta:
